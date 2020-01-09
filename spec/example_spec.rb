ca29beb1fbf87e2bb984b60d048870a42b3adb76
Ian Candy <iancandysemail@gmail.com>
2020-01-09 16:29:13 -0500

    add tests so submission is required

---
+++ spec/example_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe 'These examples' do
+  it 'runs' do
+    expect("Hello").to be_truthy
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,47 @@
+require_relative '../test'
+require_relative '../app'
+
+RSpec.configure do |config|
+  # rspec-expectations config goes here. You can use an alternate
+  # assertion/expectation library such as wrong or the stdlib/minitest
+  # assertions if you prefer.
+  config.expect_with :rspec do |expectations|
+    # This option will default to `true` in RSpec 4. It makes the `description`
+    # and `failure_message` of custom matchers include text for helper methods
+    # defined using `chain`, e.g.:
+    #     be_bigger_than(2).and_smaller_than(4).description
+    #     # => "be bigger than 2 and smaller than 4"
+    # ...rather than:
+    #     # => "be bigger than 2"
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  # rspec-mocks config goes here. You can use an alternate test double
+  # library (such as bogus or mocha) by changing the `mock_with` option here.
+  config.mock_with :rspec do |mocks|
+    # Prevents you from mocking or stubbing a method that does not exist on
+    # a real object. This is generally recommended, and will default to
+    # `true` in RSpec 4.
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+end
+
+# To parse and test an HTML file, add 'nokogiri' to Gemfile and uncomment these methods
+# def html_file_contents
+#   File.read('./index.html')
+# end
+#
+# def parsed_html
+#   Nokogiri::HTML(html_file_contents) do |config|
+#     config.strict.dtdload.dtdvalid.noblanks
+#   end
+# end
+
+# To parse and test a CSS file, add 'css_parser' to Gemfile and uncomment this method
+# def parsed_css
+#   parser = CssParser::Parser.new
+#   parser.load_uri!('./style.css')
+#   parser
+# end
