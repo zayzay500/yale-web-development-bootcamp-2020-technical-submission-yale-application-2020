ebfba7856aac0c8fc35bc0ec120269a835f71b8e
Ian Candy <iancandysemail@gmail.com>
2020-01-13 13:42:44 -0500

    remove solution

--- app.rb
+++ app.rb
@@ -1,15 +1,8 @@
 require_relative './test'
 AVERAGE_HEIGHT_IN_INCHES = 67
 
-name = "Ian Candy"
-age = 34
-favorite_food = "Pizza"
-height_in_inches = 76
+# Your Code Here!
 
-height_message = height_in_inches > AVERAGE_HEIGHT_IN_INCHES ? "You are taller than average by #{height_in_inches - AVERAGE_HEIGHT_IN_INCHES} inches" : "You are not taller than average."
-
-
-
-## Do not modify
+## Do not modify below this line!
 
 output(name, age, favorite_food, height_message)
