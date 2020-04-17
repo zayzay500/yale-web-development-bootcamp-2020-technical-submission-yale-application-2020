cdd2db3e7d07a7da8b32df511e4edb36c68ed2f1
Lindsay Pierce <lindsay.pierce@yale.edu>
2020-04-17 04:14:24 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,6 +3,12 @@ AVERAGE_HEIGHT_IN_INCHES = 67
 
 # Your Code Here!
 
+name = "Lindsay Pierce"
+age = 19
+favorite_food = "a hungry man hero from gheav"
+height_in_inches = 70
+height_message = (height_in_inches > AVERAGE_HEIGHT_IN_INCHES) ? "You are taller than average." : "You are not taller than average."
+
 ## Do not modify below this line!
 
 output(name, age, favorite_food, height_message)
