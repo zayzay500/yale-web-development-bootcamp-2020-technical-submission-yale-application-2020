83cd6eebd49807173f2e6c9cc122442ab1845e90
Lindsay Pierce <lindsay.pierce@yale.edu>
2020-04-17 04:13:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,9 +5,9 @@ AVERAGE_HEIGHT_IN_INCHES = 67
 
 name = "Lindsay Pierce"
 age = 19
-favorite_food = "hungry man hero"
+favorite_food = "a hungry man hero from gheav"
 height_in_inches = 70
-height_message = (height_in_inches > AVERAGE_HEIGHT_IN_INCHES) ? "You are taller than average" : "You are not taller than average"
+height_message = (height_in_inches > AVERAGE_HEIGHT_IN_INCHES) ? "You are taller than average." : "You are not taller than average."
 
 ## Do not modify below this line!
 
